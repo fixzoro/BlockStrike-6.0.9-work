199dab0897309f3db4ae1bc6e0871c9304867494
Mark <130450162+Drrixs@users.noreply.github.com>
2024-02-09 21:50:24 +0300

    Create Soon.cs

---
+++ Soon.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Здесь скоро будут рабочие исходники");
+        Console.WriteLine("There will be working sources here soon");
+    }
+}
